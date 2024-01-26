ac95510e13f54051868367c06fc84fe591a5b5a6
Irina Saveleva <goyalion17@gmail.com>
2024-01-26 16:13:45 +0100

    Vite + React, SPA, React Router, Link/NavLink, Styling: css modules

---
+++ src/component/AppNav.jsx
@@ -0,0 +1,5 @@
+import { nav } from "./AppNav.module.css";
+
+export default function AppNav() {
+  return <nav className={nav}>App navigation</nav>;
+}
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,10 @@
+import AppNav from "../component/AppNav";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
