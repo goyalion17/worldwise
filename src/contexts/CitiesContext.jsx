af458345047f065daecaccda6b5ca83b41c3d083
Irina Saveleva <goyalion17@gmail.com>
2024-02-04 14:00:21 +0100

    useCallback for function getCity fixes multiple requests problem

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -70,7 +76,7 @@ function CitiesProviders({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
+  const getCity = useCallback(async function getCity(id) {
     if (Number(id) === currentCity.id) return;
 
     dispatch({ type: "loading" });
@@ -85,7 +91,7 @@ function CitiesProviders({ children }) {
         payload: "There was an error loading the city...",
       });
     }
-  }
+  }, [currentCity.id]);
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
